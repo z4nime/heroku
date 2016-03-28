f9898ca580269f4dd42bd69b9799e9fb784b335e
z4nime <boybrandla@hotmail.com>
2016-03-29 02:47:33 +0700

    upload heroku

---
+++ public/js/main.js
@@ -0,0 +1,503 @@
+var app = angular.module('main', ['ui.router','ipCookie','ngCkeditor','imageupload','angular-jwplayer'])
+app.config(function($stateProvider, $urlRouterProvider){
+  $urlRouterProvider.otherwise("/");
+  //
+  // Now set up the states
+  $stateProvider
+    .state('entersite', {
+      url: "/entersite",
+      templateUrl: "./templates/entersite.html"
+    })
+  $stateProvider
+    .state('/', {
+      url: "/",
+      templateUrl: "./templates/portal.html",
+      controller: 'Index as ctrl'
+    })
+  $stateProvider
+    .state('register', {
+      url: "/register",
+      templateUrl: "./templates/register.html",
+      controller: 'Register as ctrl'
+    })
+  $stateProvider
+    .state('backend', {
+      url: "/backend",
+      templateUrl: "./backend/index.html",
+      controller: 'Backend as ctrl'
+    })
+  $stateProvider
+    .state('editanime', {
+      url: "/backend/anime/:id",
+      templateUrl: "./backend/editAnime.html",
+      controller: 'EditAnime as ctrl'
+    })
+  $stateProvider
+    .state('anime', {
+      url: "/anime/:id",
+      templateUrl: "./templates/anime.html",
+      controller: 'viewAnime as ctrl'
+    })
+  
+});
+
+
+
+app.controller('Register', ['$scope', '$http' , '$state' ,'ipCookie', function($scope, $http, $state, ipCookie){
+  var ctrl = this;
+  ctrl.checkOppainame = function(oppai_name){
+    if(oppai_name==undefined)
+      $scope.namePass = 'default';
+    else
+    {
+      $http.get("/api/users/"+oppai_name)
+      .success(function(data){
+        if(data!='')
+          $scope.namePass = false;
+        else
+          $scope.namePass = true; 
+      }); 
+    }
+    
+  };
+  ctrl.checkEmail = function(email){
+    $http.get("/api/users/")
+    .success(function(data){
+      if(email==undefined)
+        $scope.emailPass = 'default';
+      else{
+        if(data =='')
+          $scope.emailPass = true;
+        else
+        {
+          for(var i=0;i<data.length;i++)
+          {
+            if(email==data[i].email)
+            {
+              $scope.emailPass = false;
+              break;
+            }
+            else
+              $scope.emailPass = true;
+          }
+        }
+
+      }
+    });
+  };
+  ctrl.Register = function(data){
+    var hashPassword = calcSHA1(data.password);
+    data.password = hashPassword;
+    $http.post("/api/users/register/",data)
+    .then(function successCallback(response) {
+      //console.log(response.data)
+    }, function errorCallback(response) {
+      
+    });
+    $scope.message = "Register successful please wait redirect. . .";
+    setTimeout(function () {
+          $scope.$apply(function () {
+        ipCookie("cookieLogin", data, { expires: 15 });
+              $state.go("/");
+          });
+      }, 3000);
+  }
+
+  $scope.back = function(){
+    window.history.back();
+  }
+}]);
+
+app.controller('Index', ['$scope' ,'$http' , '$state' , 'ipCookie' , function($scope,$http,$state,ipCookie){
+  var ctrl = this;
+  ctrl.init = function(){
+    ctrl.greeting = 'Welcome!';
+    $scope.remember = true;
+    if(ipCookie("cookieLogin"))
+    {
+      $http.get("/api/users/"+ipCookie("cookieLogin").oppai_name)
+      .success(function(data){
+        ctrl.userinfo = true;
+        ctrl.admin = data[0].permission;
+        ctrl.name = data[0].oppai_name;
+        ctrl.avatar = data[0].avatar;
+        $state.go("/");
+      })
+    }
+    $http.get('/api/anime')
+    .success(function(data){
+      ctrl.anime = data;
+    });
+    setInterval(function(){
+      $http.get('/api/anime')
+      .success(function(data){
+        ctrl.anime = data;
+      });
+    },60000);
+    
+  }
+    ctrl.login = function(data){
+      var hashPassword = calcSHA1(data.password);
+    $http.post("/api/login/",data)
+    .then(function successCallback(response) {
+      if(response.data[0] != undefined)
+      {
+        if(response.data[0].password == hashPassword)
+        {
+          if($scope.remember === true)
+          {
+            ctrl.userinfo = true;
+            ctrl.admin = response.data[0].permission;
+              ctrl.name = response.data[0].oppai_name;
+              ctrl.avatar = response.data[0].avatar;
+              ipCookie("cookieLogin", response.data[0], { expires: 15 });
+            }
+            else
+            {
+              ctrl.userinfo = true;
+              ctrl.admin = response.data[0].permission;
+              ctrl.name = response.data[0].oppai_name;
+              ctrl.avatar = response.data[0].avatar;
+              ipCookie("cookieLogin", response.data[0], { expires: 1 });
+            }
+        }
+        else
+        {
+          $scope.data.password = null;
+          $scope.passwordPass = false;
+          $scope.emailPass = true;
+          setTimeout(function () {
+                  $scope.$apply(function () {
+              $scope.passwordPass = 'default';
+                  });
+              }, 2000);
+        }
+      }
+      else
+      {
+        $scope.emailPass = false;
+        $scope.data.password = null;
+        $scope.data.email = null;
+        setTimeout(function () {
+              $scope.$apply(function () {
+            $scope.emailPass = 'default';
+              });
+          }, 2000);
+      }
+
+      
+    }, function errorCallback(response) {
+      console.log(response.message)
+    });
+  }
+  $scope.logout = function(){
+    ctrl.userinfo = false;
+    ctrl.name = null;
+    ctrl.avatat = null;
+    ipCookie.remove("cookieLogin");
+    $state.go("/");
+  }
+
+
+}]);
+
+
+
+app.controller('Backend', ['$scope' ,'$http' , '$state' , 'ipCookie' , function($scope,$http,$state,ipCookie){
+  var ctrl = this;
+  ctrl.init = function(){
+    $scope.data = {
+      availableOptions: [
+        {id: '1', name: 'ยังไม่จบ'},
+        {id: '2', name: 'จบแล้ว'}
+      ],
+      status: {id: '1', name: 'ยังไม่จบ'} //This sets the default value of the select in the ui
+    };
+
+    if(ipCookie("cookieLogin"))
+    {
+      $http.get("/api/users/"+ipCookie("cookieLogin").oppai_name)
+        .success(function(data){
+          if(data[0].permission <4)
+            $state.go("/");
+          else
+          {
+            ctrl.hello = "Hi ~~~  " + data[0].oppai_name;
+            // temp 
+            var currentdate = new Date(); 
+            var datetime = currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();            
+            $http.get('https://api.ipify.org?format=json')
+            .success(function(ip){
+              var temp = {"oppai_name":ipCookie("cookieLogin").oppai_name,
+              "action":"Login Backend By : "+data[0].oppai_name,
+              "ip":ip.ip,
+              "date_time":datetime};
+              $http.post('/api/temp',temp)
+              .success(function(data){
+                ctrl.temp();
+              });
+            });
+          }
+        });
+    }
+    ctrl.anime();
+    ctrl.limit = 0;
+    ctrl.total_more = 0;
+  }
+  ctrl.hover = function(){
+    var audio = new Audio('/sound/Pop.mp3');
+    audio.volume = 0.1;
+    audio.play();
+  };
+
+  ctrl.post = function(data){
+    var formData = new FormData();
+    formData.append('image', data.cover.file, data.cover.file.name);
+    $http.post('/api/upload/cover_anime', formData, {
+      headers: { 'Content-Type': false },
+      transformRequest: angular.identity
+    }).success(function(result) {
+        data.cover = result.path
+        $http.post('/api/anime/create',data) // insert anime
+        .then(function successCallback(response){
+          $scope.data.topic = null;
+          $scope.data.detail = null;
+          $scope.data.cover = null;
+          ctrl.anime();
+
+        })
+    }); 
+
+    // temp 
+    var currentdate = new Date(); 
+    // var datetime = "Last Sync: " + currentdate.getDate() + "/"
+    //             + (currentdate.getMonth()+1)  + "/" 
+    //             + currentdate.getFullYear() + " @ "  
+    var datetime = currentdate.getHours() + ":"  
+        + currentdate.getMinutes() + ":" 
+        + currentdate.getSeconds();          
+    $http.get('https://api.ipify.org?format=json')
+    .success(function(ip){
+      var temp = {"oppai_name":ipCookie("cookieLogin").oppai_name,
+      "action":"Create Anime : "+data.topic,
+      "ip":ip.ip,
+      "date_time":datetime};
+      $http.post('/api/temp',temp)
+      .success(function(data){
+
+      });
+    });
+  };
+
+  ctrl.anime = function(){
+    $http.get('/api/anime')
+    .success(function(data){
+      ctrl.allAnime = data;
+    });
+  }
+
+  ctrl.select_anime = function(anime_id,name){
+    ctrl.showEpisode = true;
+    ctrl.anime_name = name;
+    $scope.anime_id = anime_id;
+    $scope.search_anime = "";
+    ctrl.getEpisode(anime_id);
+
+    $scope.episode = {
+      availableOptions: [
+        {id: '1', name: 'facebook'},
+        {id: '2', name: 'googleDrive'}
+      ],
+      type: {id: '1', name: 'facebook'} //This sets the default value of the select in the ui
+    };
+  }
+  ctrl.getEpisode = function(anime_id){
+    $http.get('/api/anime/episode/'+anime_id)
+    .success(function(data){
+      ctrl.episode = data;
+    });
+  }
+  ctrl.delete = function(anime_id,ep){
+    $http.delete("/api/anime/episode/del/"+anime_id+"/"+ep)
+    .success(function(data){
+      ctrl.getEpisode($scope.anime_id);
+    });
+    // temp 
+    var currentdate = new Date(); 
+    var datetime = currentdate.getHours() + ":"  
+        + currentdate.getMinutes() + ":" 
+        + currentdate.getSeconds();            
+    $http.get('https://api.ipify.org?format=json')
+    .success(function(ip){
+      var temp = {"oppai_name":ipCookie("cookieLogin").oppai_name,
+      "action":"Delete Anime : "+ctrl.anime_name+ " EP : "+ep,
+      "ip":ip.ip,
+      "date_time":datetime};
+      $http.post('/api/temp',temp)
+      .success(function(data){
+        // console.log(data)
+      });
+    });
+  };
+  ctrl.post_ep = function(data){
+    var obj = [];
+    obj.push({"anime_id":$scope.anime_id,"ep":data.ep,"type":data.type,"url":data.url});
+    $http.post('/api/anime/episode/',obj)
+    .success(function(data){
+      ctrl.getEpisode($scope.anime_id);
+    });
+
+    // temp 
+    var currentdate = new Date(); 
+    var datetime = currentdate.getHours() + ":"  
+        + currentdate.getMinutes() + ":" 
+        + currentdate.getSeconds();            
+    $http.get('https://api.ipify.org?format=json')
+    .success(function(ip){
+      var temp = {"oppai_name":ipCookie("cookieLogin").oppai_name,
+      "action":"Update Anime : "+ctrl.anime_name+ " EP : "+data.ep,
+      "ip":ip.ip,
+      "date_time":datetime};
+      $http.post('/api/temp',temp)
+      .success(function(data){
+        // console.log(data)
+      });
+    });
+  }
+
+  ctrl.temp = function(){
+    $http.get('/api/temp')
+    .success(function(data){
+      ctrl.data_temp = data;
+      ctrl.limit=ctrl.limit+20;
+      if(ctrl.total_more>=0)
+        ctrl.total_more = ctrl.data_temp.length-ctrl.limit;
+      if(ctrl.total_more<0)
+        ctrl.total_more =0;
+    });
+  }
+  ctrl.clear = function(){
+    if(ipCookie("cookieLogin").permission<5)
+      alert("Access Deny By WebMaster");
+    else{
+      $http.delete('/api/temp')
+      .success(function(data){
+        ctrl.temp();
+      });
+    }
+  }
+
+}]);
+
+
+
+app.controller('EditAnime', ['$scope' ,'$http' , '$state' , 'ipCookie' ,'$stateParams', function($scope,$http,$state,ipCookie,$stateParams){
+  var ctrl = this;
+  ctrl.init = function(){
+   if(ipCookie("cookieLogin"))
+    {
+      $http.get("/api/users/"+ipCookie("cookieLogin").oppai_name)
+        .success(function(data){
+          if(data[0].permission <4)
+            $state.go("/");
+          else
+            ctrl.hello = "Hi ~~~ " + data[0].oppai_name;
+        });
+    }
+    $http.get('/api/anime/'+$stateParams.id)
+    .success(function(data){
+      $scope.data = data[0];
+    })
+  }
+
+  ctrl.edit = function(data){
+    // temp 
+    var currentdate = new Date(); 
+    var datetime = currentdate.getHours() + ":"  
+        + currentdate.getMinutes() + ":" 
+        + currentdate.getSeconds();            
+    $http.get('https://api.ipify.org?format=json')
+    .success(function(ip){
+      var temp = {"oppai_name":ipCookie("cookieLogin").oppai_name,
+      "action":"Edit Anime : "+data.topic,
+      "ip":ip.ip,
+      "date_time":datetime};
+      $http.post('/api/temp',temp)
+      .success(function(data){
+        // console.log(data)
+      });
+    });
+
+    $http.put('/api/anime/',data)
+    .success(function(data){
+      //window.location.reload();
+    });
+    
+  }
+  ctrl.delete = function(anime_id,topic){
+    // temp 
+    var currentdate = new Date(); 
+    var datetime = currentdate.getHours() + ":"  
+        + currentdate.getMinutes() + ":" 
+        + currentdate.getSeconds();            
+    $http.get('https://api.ipify.org?format=json')
+    .success(function(ip){
+      var temp = {"oppai_name":ipCookie("cookieLogin").oppai_name,
+      "action":"Delete Anime : "+topic,
+      "ip":ip.ip,
+      "date_time":datetime};
+      $http.post('/api/temp',temp)
+      .success(function(data){
+        // console.log(data)
+      });
+    });
+
+    $http.delete("/api/anime/"+anime_id)
+    .success(function(data){
+      window.history.back();
+    });
+  };
+  ctrl.back = function(){
+    window.history.back();
+  }
+
+}]);
+
+app.controller('viewAnime',function($scope,$http,$stateParams){
+  var ctrl = this;
+  ctrl.init = function(){
+    $http.get('/api/anime/'+$stateParams.id)
+    .success(function(data){
+      $scope.data = data[0];
+      $("#detail").append(data[0].detail)
+    })
+    $http.get('/api/anime/episode/'+$stateParams.id)
+    .success(function(data){
+      ctrl.episode = data;
+    });
+  }
+  $scope.options = {
+    playlist: [
+    {
+      file: "https://c8379c8de7eca3a0ffb5fb4fbe0c343a266b1cba.googledrive.com/host/0B9OjzW_2GIRYM254cFJjcWxsdEk/Hidan no Aria AA/%5BInuga-FS%5D%20Hidan%20no%20Aria%20AA%20-%2001%20%5BTV%20720P%2060FPS%20X264%20AAC%5D%20%5BBC42E86D%5D.mp4",
+      image: "./data/cover_anime/upload_0344f2a951aebb6b966958c557c80951.jpg", // optionnal
+      title: 'World Trigger เวิลด์ ทริกเกอร์ 70',
+      mediaid: '50'
+    },
+    {
+      file: "https://c8379c8de7eca3a0ffb5fb4fbe0c343a266b1cba.googledrive.com/host/0B9OjzW_2GIRYM254cFJjcWxsdEk/Hidan no Aria AA/%5BInuga-FS%5D%20Hidan%20no%20Aria%20AA%20-%2001%20%5BTV%20720P%2060FPS%20X264%20AAC%5D%20%5BBC42E86D%5D.mp4",
+      image: "./data/cover_anime/upload_0344f2a951aebb6b966958c557c80951.jpg", // optionnal
+      title: 'World Trigger เวิลด์ ทริกเกอร์ 70',
+      mediaid: '51'
+    },
+    {
+      file: "https://c8379c8de7eca3a0ffb5fb4fbe0c343a266b1cba.googledrive.com/host/0B9OjzW_2GIRYM254cFJjcWxsdEk/Hidan no Aria AA/%5BInuga-FS%5D%20Hidan%20no%20Aria%20AA%20-%2001%20%5BTV%20720P%2060FPS%20X264%20AAC%5D%20%5BBC42E86D%5D.mp4",
+      image: "./data/cover_anime/upload_0344f2a951aebb6b966958c557c80951.jpg", // optionnal
+      title: 'World Trigger เวิลด์ ทริกเกอร์ 70',
+      mediaid: '52'
+    }]
+  }
+
+});
---
+++ server.js
@@ -0,0 +1,208 @@
+'use strict';
+
+// simple express server
+var express = require('express');
+var app = express();
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
+var busboy = require('connect-busboy'); //middleware for form/file upload
+var formidable = require('formidable');
+var path = require('path');     //used for file path
+var fs = require('fs-extra');       //File System - for file manipulation
+
+//var users = require('./api/users');
+// var db_config = {
+//   	host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'anime'
+// };
+// var db;
+
+// time 
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0!
+var yyyy = today.getFullYear();
+
+if(dd<10) {
+    dd='0'+dd
+} 
+
+if(mm<10) {
+    mm='0'+mm
+} 
+
+today = dd+'-'+mm+'-'+yyyy;
+
+// function handleDisconnect() {
+//   	db = mysql.createConnection(db_config); // Recreate the connection, since
+//                                                   // the old one cannot be reused.
+//   	db.connect(function(err) {              // The server is either down
+// 	    if(err) {                                     // or restarting (takes a while sometimes).
+// 	      	console.log('error when connecting to db');
+// 	      	setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+// 	    }                                     // to avoid a hot loop, and to allow our node script to
+//   	});                                     // process asynchronous requests in the meantime.
+//                                           // If you're also serving http, display a 503 error.
+//   	db.on('error', function(err) {
+// 	    console.log('Connection Lost');
+// 	    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+// 	      	handleDisconnect();                         // lost due to either server restart, or a
+// 	    } else {                                      // connnection idle timeout (the wait_timeout
+// 	      	throw err;                                  // server variable configures this)
+// 	    }
+//   	});
+// }
+
+// handleDisconnect();
+
+app.use('/', express.static('public'));
+app.use(busboy());
+app.use(express.static(path.join(__dirname, 'public')));
+//parse application/json
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// user
+app.get('/api/users/', function(req, res,next) {
+	db.query('select * from user', function(err, rows, fields) {
+	  	if (err) throw err;
+		  //res.send("data");
+		  res.json(rows);
+
+	});
+});
+app.get('/api/users/:oppai_name', function(req, res) {
+	db.query("select * from user where oppai_name='"+req.params.oppai_name+"'", function(err, rows, fields) {
+	  	if (!req.params) 
+	 		return res.sendStatus(400);
+	 	else res.json(rows);
+
+	});
+});
+app.post('/api/login', function(req, res) {
+	db.query("select * from user where email='"+req.body.email+"'", function(err, rows, fields) {
+	  	if (!rows) 
+	 		return res.send("not result");
+	 	else res.json(rows);
+
+	});
+});
+app.post('/api/users/register/',function(req, res) {
+	db.query("insert user set oppai_name='"+req.body.oppai_name+"', email='"+req.body.email+"', password='"+req.body.password+"' , avatar='http://data.whicdn.com/images/175676560/superthumb.jpg' , permission=1", function(err, rows, fields) {
+	 	if (!req.body) 
+	 		return res.sendStatus(400);
+	 	else res.json(req.body);
+	});
+});
+
+// anime 
+app.post('/api/upload/cover_anime',function(req,res){
+	var form = new formidable.IncomingForm();
+    //Formidable uploads to operating systems tmp dir by default
+    form.uploadDir = "./public/data/cover_anime/";       //set upload directory
+    form.keepExtensions = true;     //keep file extension
+
+    form.parse(req, function(err, fields, files) {
+        // console.log("file size: "+JSON.stringify(files.fileUploaded.size));
+        // console.log("file path: "+JSON.stringify(files.fileUploaded.path));
+        // console.log("file name: "+JSON.stringify(files.fileUploaded.name));
+        // console.log("file type: "+JSON.stringify(files.fileUploaded.type));
+        // console.log("astModifiedDate: "+JSON.stringify(files.fileUploaded.lastModifiedDate));
+
+        // //Formidable changes the name of the uploaded file
+        // //Rename the file to its original name
+        // fs.rename(files.fileUploaded.path, './public/data/cover_anime/'+files.fileUploaded.name, function(err) {
+        // if (err)
+        //     throw err;
+        //   console.log('renamed complete');  
+        // });
+        //res.redirect('back');
+        var modifieName = files.image.path.split("\\");
+        files.image.path = "/"+modifieName[1]+"/"+modifieName[2]+"/"+modifieName[3];
+        db.query("insert image_oppai set name='"+files.image.name+"', path='"+files.image.path+"', create_image='"+today+"'", function(err, rows, fields) {
+        	res.json(files.image)
+		});
+        
+    });
+});
+
+app.get('/api/anime', function(req, res) {
+	db.query('select * from anime', function(err, rows, fields) {
+	  	if (err) throw err;
+		 res.json(rows);
+	});
+});
+app.get('/api/anime/:id', function(req, res) {
+	db.query("select * from anime where anime_id='"+req.params.id+"'", function(err, rows, fields) {
+	  	if (err) throw err;
+		 res.json(rows);
+	});
+});
+app.post('/api/anime/create',function(req,res){
+  db.query("insert anime set topic='"+req.body.topic+"', cover_path='"+req.body.cover+"', status='"+req.body.status.name+"' ,detail='"+req.body.detail+"' ,update_time='"+today+"'", function(err, rows, fields) {
+    if (!req.body) 
+      return res.sendStatus(400);
+    else res.json(req.body.data);
+  });
+});
+app.put('/api/anime/', function(req, res) {
+	db.query("update anime set topic='"+req.body.topic+"' , status='"+req.body.status+"' , detail='"+req.body.detail+"' , update_time='"+today+"' where anime_id='"+req.body.anime_id+"'", function(err, rows, fields) {
+	  	if (err) throw err;
+		res.json(rows);
+	});
+});
+app.delete('/api/anime/:anime_id',function(req,res){
+  db.query("delete from anime where anime_id='"+req.params.anime_id+"'",function(err,rows,fileds){
+    if (err) throw err;
+     res.json(rows);
+  })
+});
+
+
+// episode
+app.post('/api/anime/episode/',function(req,res){
+	db.query("insert episode set anime_id='"+req.body[0].anime_id+"', ep='"+req.body[0].ep+"', url='"+req.body[0].url+"', type='"+req.body[0].type+"'",function(err,rows,fields){
+		if(!req.body)
+			return res.sendStats(400);
+		else res.json(req.body[0].data);
+	});
+});
+app.get('/api/anime/episode/:id',function(req,res){
+	db.query("select * from episode where anime_id='"+req.params.id+"'",function(err,rows,fileds){
+		if (err) throw err;
+		 res.json(rows);
+	})
+});
+app.delete('/api/anime/episode/del/:anime_id/:ep',function(req,res){
+  db.query("delete from episode where anime_id='"+req.params.anime_id+"' and ep='"+req.params.ep+"' ",function(err,rows,fileds){
+    if (err) throw err;
+     res.json(rows);
+  })
+});
+
+//temp
+app.post('/api/temp',function(req,res){
+  db.query("insert temp set oppai_name='"+req.body.oppai_name+"', action='"+req.body.action+"', ip='"+req.body.ip+"', date='"+today+"',date_time='"+req.body.date_time+"'",function(err,rows,fields){
+    if(!req.body)
+      return res.sendStats(400);
+    else res.json(req.body);
+  });
+});
+app.get('/api/temp',function(req,res){
+  db.query("select * from temp",function(err,rows,fields){
+    if (err) throw err;
+      res.json(rows);
+  });
+});
+app.delete('/api/temp',function(req,res){
+  db.query("DELETE FROM temp WHERE 1",function(err,rows,fields){
+    if (err) throw err;
+      res.json(rows);
+  });
+});
+app.listen(8888); 
+//console.log("Server on port : 3000")
